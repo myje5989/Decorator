1290d12ccf5b13f07960f8c956a41d655c4b0039
myje5989 <30823860+myje5989@users.noreply.github.com>
2017-08-15 16:11:30 +0700

    add Espresso Class and HouseBlend Class

---
+++ src/Espresso.java
@@ -0,0 +1,10 @@
+
+public class Espresso extends Beverage {
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/HouseBlend.java
@@ -0,0 +1,10 @@
+
+public class HouseBlend extends Beverage {
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
