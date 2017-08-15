e4121d9c9b8f953552c539a5cb58050a82aee5e7
myje5989 <30823860+myje5989@users.noreply.github.com>
2017-08-15 16:13:23 +0700

    ..

--- src/HouseBlend.java
+++ src/HouseBlend.java
@@ -1,6 +1,8 @@
 
 public class HouseBlend extends Beverage {
-
+public HouseBlend(){
+	description = "House Blend Coffee";
+}
 	@Override
 	public double cost() {
 		// TODO Auto-generated method stub
